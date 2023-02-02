813de57e61de55ad7fb34da35a23c7a94cf63131
Sergey <gemodialez71@gmail.com>
2023-02-02 14:37:01 +0300

    end 2.3.4

--- 2.3.4.c
+++ 2.3.4.c
@@ -1,9 +1,46 @@
+/*
+Знак числа
+В математике функция sign(x) (знак числа) определена так: 
+sign(x) = 1,   если x > 0, 
+sign(x) = -1, если x < 0, 
+sign(x) = 0,   если x = 0.
+
+Для данного числа x выведите значение sign(x).
+
+Входные данные
+
+Вводится число x.
+
+Выходные данные
+
+Выведите ответ на задачу.
+
+Sample Input:
+
+115
+Sample Output:
+
+1
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
-	printf("%s", "Hello");
+	int	n = 0;
+	scanf("%d", &n);
+	if (n < 0) {
+		printf("%d\n", -1);	
+	}
+	else if (n > 0) {
+		printf("%d\n", 1);	
+	}else {
+		printf("%d\n", 0);	
+	}	
+	
 
 	return 0;
 }
