0441ad347592d3b2acb5c39e5f556799aa01fa75
Sergey <gemodialez71@gmail.com>
2023-02-02 15:01:11 +0300

    theEnd

--- 2.3.4.c
+++ 2.3.4.c
@@ -32,14 +32,16 @@ int main()
 {
 	int	n = 0;
 	scanf("%d", &n);
-	if (n < 0) {
-		printf("%d\n", -1);	
-	}
-	else if (n > 0) {
-		printf("%d\n", 1);	
-	}else {
-		printf("%d\n", 0);	
-	}	
+
+	printf("%d", n>0 ? 1 : n < 0 ? -1 : 0);
+	// if (n < 0) {
+	// 	printf("%d\n", -1);	
+	// }
+	// else if (n > 0) {
+	// 	printf("%d\n", 1);	
+	// }else {
+	// 	printf("%d\n", 0);	
+	// }	
 	
 
 	return 0;
